24a551c73abead3dc5a96780253e8340eae957e6
Ivan Polivkin <polivkin.andrew@mail.ru>
2023-08-10 18:31:17 +0300

    Everything_work

--- student.py
+++ student.py
@@ -1,3 +1,13 @@
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def __str__(self):
+        pes = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return pes
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -8,11 +18,11 @@ class Student:
         self.grades = {}
 
     def __str__ (self):
-        res = f'Имя: {self.name} Фамилия: {self.surname} Средння оценка за домашнее задание: {self.grades} Курсы в процессе изучения: {self.courses_in_progress} Завершенные курсы: {self.finished_courses}'
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредння оценка за домашнее задание: {self.grades}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}'
         return res
     
     def rate_L_M(self, lektor, lec, grade):
-        if isinstance(lektor, Lekturer) and lec in self.lec:
+        if isinstance(lektor, Lekturer) and lec in lektor.lec:
             if lec in lektor.grades:
                 lektor.grades[lec] += [grade]
             else:
@@ -20,26 +30,16 @@ class Student:
         else:
             return 'Ошибка'
 
-class Mentor:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-    def __str__(self):
-        pes = f'Имя: {self.name} Фамилия: {self.surname}'
-        return pes
-
 class Lekturer (Mentor):
-    def init(self, name, surname):
-        super().init(name, surname)
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
         self.name = name
         self.surname = surname
         self.lec = []
         self.grades = {}
 
     def __str__(self):
-        ses = f'Имя: {self.name} Фамилия: {self.surname} Средняя оценка за лекции: {self.grades}'
+        ses = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.grades}'
         return ses
 
 class Reviewer (Mentor):      
@@ -66,20 +66,38 @@ student_3.finished_courses += ['Git']
 mentor_1 = Reviewer('Some', 'Buddy')
 mentor_1.courses_attached += ['Python', 'Git']
 
+mentor_2 = Reviewer('Every', 'One')
+mentor_2.courses_attached += ['Python', 'Git']
+
 lekturer_1 = Lekturer('Pierce', 'Brosnan') # добавление в список преподаваемых курсов
-# lekturer_1.lec += ['Python', 'Git']
+lekturer_1.lec += ['Python', 'Git']
 
- 
+# выставление оценок студентам 
 mentor_1.rate_hw(student_1, 'Python', 10)
+mentor_2.rate_hw(student_1, 'Python', 8)
+mentor_1.rate_hw(student_1, 'Python', 8)
 mentor_1.rate_hw(student_2, 'Git', 8)
+mentor_2.rate_hw(student_2, 'Git', 7)
+mentor_1.rate_hw(student_2, 'Git', 5)
 mentor_1.rate_hw(student_3, 'Python', 7)
-# student_1.rate_L_M(lekturer_1, 'Python', 10)
- 
-print(student_1.grades)
-print(student_2.grades)
-print(student_3.grades)
 
-print(student_2)
-# print(lekturer_1)
-print(mentor_1)
+#выставление оценок лекторам
+student_1.rate_L_M(lekturer_1, 'Python', 2)
+student_1.rate_L_M(lekturer_1, 'Python', 5)
+student_1.rate_L_M(lekturer_1, 'Python', 6)
+student_1.rate_L_M(lekturer_1, 'Python', 10)
+student_2.rate_L_M(lekturer_1, 'Git', 9)
+student_2.rate_L_M(lekturer_1, 'Git', 9)
+student_2.rate_L_M(lekturer_1, 'Git', 10)
+
+# Это работает
+# print(student_1.grades)
+# print(student_2.grades)
+# print(student_3.grades)
+# print(student_2)
+# print(mentor_1)
+# print(mentor_2)
+
+# Это не работает
 # print(student_1 > student_3)
+print(lekturer_1)
